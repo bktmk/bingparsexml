3ed592b7b544b66e039304087f5964cdc1b4cdc7
bktmk <47651927+bktmk@users.noreply.github.com>
2019-03-05 16:47:38 +1100

    Update array test.py

--- array test.py
+++ array test.py
@@ -1,38 +1,60 @@
 import re
 import sys
 
-outputfile = open(r'out.txt',"w+", encoding="utf-8")
+
+filename = r'c:\temp\bingfeed.xml'
+xmlfile = open(filename,encoding='utf8')
+
+outputfile = open(r'c:\temp\out4.tsv',"w+", encoding="utf-8")
 
 
-farray = """uploadts-1550147989-sec_products_06.xml
-uploadts-1550143579-sec_products_03.xml
-uploadts-1550143903-sec_products_04.xml
-uploadts-1550144018-sec_products_05.xml
-uploadts-1550144049-sec_products_02.xml
-uploadts-1550144197-sec_products_01.xml
-uploadts-1550147275-sec_products_08.xml
-uploadts-1550147754-sec_products_09.xml
-uploadts-1550147842-sec_products_10.xml
-uploadts-1550147970-sec_products_07.xml""".split("\n")
+def saveline(string):
+    if (string == "end"):
+        outputfile.write("\n")
+    else:
+        #print(string,end='')
+        outputfile.write(string)
+
+
+#header = "sku	condition	title	price	new	type	image	sku	googlecat	available	landingpage	country	price1	price2	price3	price4\n"
+#saveline(header)
+
+saveline("sku	condition	description	link	price	title	cateory	cover	sku	type	availability	product	au	priceau	nz	pricenz\n")
+
+i=0
+start=0
+max=1000
+
+for line in xmlfile:
+    i=i+1
     
-regex = "<link>https://www.angusrobertson.com.au/(.*?)(\d{13})</link>"
-
-def inspect(filename):
-#    print(filename)
-    xmlfile = open(filename,encoding='utf8')
-    for line in xmlfile:
-        matches = re.findall(regex,line)
-        if (matches):
-            for m in matches:
-                printline = filename + "\t" + m[1]+"\t"+m[0]+"\n"
-                outputfile.write(printline)
-    xmlfile.close
-    outputfile.close
-                    
-
-for file in farray:
-    inspect(file)
+    line = line.replace('&gt;','>')
+    line = line.replace('&lt;','<')
     
-
-
+    if (line.strip() == '<entry>'):
+        start=start+1
+        next
+        
+    if (line.strip() == '</entry>'):
+        saveline("end")
+        next
+        
+    
+    regex = '<(.*?)>(.*?)</(.*?)>'
+    matches = re.findall(regex,line)
+    
+        
+    if (matches):
+        opentag = str(matches[0][0])
+        contents = str(matches[0][1])
+        closetag = str(matches[0][2])
+        
+        if(start):
+            saveline(contents + "\t")
+    
+    if i % 100000 == 0:
+        print('count ' + str(start))
+    
+    if (start>999999999 ):
+        break
 
